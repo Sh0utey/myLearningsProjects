2f3fd95817f55a9e82959e139233187eab1298ba
Sh0utey <gael.alibert1@gmail.com>
2022-01-23 12:10:06 +0100

    work on rackets movements

--- pong-game/assets/js/script.js
+++ pong-game/assets/js/script.js
@@ -1,9 +1,36 @@
 const gameBoard = document.getElementById("game-board");
-const racket1 = document.getElementById("racket1");
+const racketOne = document.getElementById("racket-one");
 const scorePlayer1 = document.getElementById("score-player1");
-const racket2 = document.getElementById("racket2");
+const racketTwo = document.getElementById("racket-two");
 const scorePlayer2 = document.getElementById("score-player2");
 const ball = document.getElementById("ball");
 const informationsBox = document.getElementById("informations-box");
+let counter = 50;
 
-const moveRacket = () => {};
+//init
+const init = (racket) => {
+  racket.style.setProperty("--top", "50%");
+};
+
+const moveUp = (racket) => {
+  racket.style.setProperty("--top", counter-- - 1 + "%");
+};
+
+const moveDown = (racket) => {
+  racket.style.setProperty("--top", counter++ + 1 + "%");
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "z") {
+    moveUp(racketOne);
+  } else if (e.key === "s") {
+    moveDown(racketOne);
+  } else if (e.key === "ArrowUp") {
+    moveUp(racketTwo);
+  } else if (e.key === "ArrowDown") {
+    moveDown(racketTwo);
+  }
+});
+
+init(racketOne);
+init(racketTwo);
